f3a0b0ac07aa393cbb51d4338f591bff9dc58441
Varadharajan <varadharajan@vegam.co>
2021-07-05 22:36:59 +0530

    Add project files.

---
+++ OracleToMySQLConvertor/DAL.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleToMySQLConvertor
+{
+    public class DAL
+    {
+        public static DataTable GetTableList(Database db)
+        {
+            StringBuilder sqlCmdBuilder = new StringBuilder();
+            sqlCmdBuilder.Append(" SELECT TABLE_NAME FROM USER_TABLES WHERE ORDER BY TABLE_NAME ");
+
+            DbCommand dbCmd = db.GetSqlStringCommand(sqlCmdBuilder.ToString());
+            dbCmd.CommandType = CommandType.Text;
+
+            return db.ExecuteDataSet(dbCmd).Tables[0];
+        }
+
+        public static DataTable GetTableColumns(Database db, string tableName)
+        {
+            StringBuilder sqlCmdBuilder = new StringBuilder();
+            sqlCmdBuilder.Append(" select col.column_id, col.owner as schema_name,col.table_name,col.column_name,col.data_type,col.data_length,col.data_precision,col.data_scale,col.nullable,col.data_default ");
+            sqlCmdBuilder.Append(" from all_tab_columns col ");
+            sqlCmdBuilder.Append(" inner join all_tables t on col.owner = t.owner and col.table_name = t.table_name ");
+            sqlCmdBuilder.Append(" where col.table_name = :TABLE_NAME ");
+            sqlCmdBuilder.Append(" order by col.column_id ");
+
+            DbCommand dbCmd = db.GetSqlStringCommand(sqlCmdBuilder.ToString());
+            dbCmd.CommandType = CommandType.Text;
+
+            db.AddInParameter(dbCmd, ":TABLE_NAME", DbType.AnsiString, tableName);
+
+            return db.ExecuteDataSet(dbCmd).Tables[0];
+        }
+
+        public static DataTable GetPrimaryKeyDetails(Database db, string tableName)
+        {
+            StringBuilder sqlCmdBuilder = new StringBuilder();
+            sqlCmdBuilder.Append(" SELECT cols.table_name, cols.column_name, cols.position, cons.status, cons.owner,cons.constraint_name ");
+            sqlCmdBuilder.Append(" FROM all_constraints cons, all_cons_columns cols ");
+            sqlCmdBuilder.Append(" WHERE cols.table_name = :TABLE_NAME AND cons.constraint_type = 'P' ");
+            sqlCmdBuilder.Append(" AND cons.constraint_name = cols.constraint_name AND cons.owner = cols.owner ");
+            sqlCmdBuilder.Append(" ORDER BY cols.table_name, cols.position ");
+
+            DbCommand dbCmd = db.GetSqlStringCommand(sqlCmdBuilder.ToString());
+            dbCmd.CommandType = CommandType.Text;
+
+            db.AddInParameter(dbCmd, ":TABLE_NAME", DbType.AnsiString, tableName);
+
+            return db.ExecuteDataSet(dbCmd).Tables[0];
+        }
+
+        public static DataTable GetUniqueKeyDetails(Database db, string tableName)
+        {
+            StringBuilder sqlCmdBuilder = new StringBuilder();
+            sqlCmdBuilder.Append(" SELECT cols.table_name, cols.column_name, cols.position, cons.status, cons.owner,cons.constraint_name ");
+            sqlCmdBuilder.Append(" FROM all_constraints cons, all_cons_columns cols ");
+            sqlCmdBuilder.Append(" WHERE cols.table_name = :TABLE_NAME AND cons.constraint_type = 'U' ");
+            sqlCmdBuilder.Append(" AND cons.constraint_name = cols.constraint_name AND cons.owner = cols.owner ");
+            sqlCmdBuilder.Append(" ORDER BY cols.table_name, cols.position ");
+
+            DbCommand dbCmd = db.GetSqlStringCommand(sqlCmdBuilder.ToString());
+            dbCmd.CommandType = CommandType.Text;
+
+            db.AddInParameter(dbCmd, ":TABLE_NAME", DbType.AnsiString, tableName);
+
+            return db.ExecuteDataSet(dbCmd).Tables[0];
+        }
+
+        public static DataTable GetIndexDetails(Database db, string tableName)
+        {
+            StringBuilder sqlCmdBuilder = new StringBuilder();
+            sqlCmdBuilder.Append(" select ind.index_name,ind_col.column_name,ind.index_type,ind.uniqueness,ind_col.column_position,ind.table_owner as schema_name,ind.table_name as object_name,ind.table_type as object_type,ind_exp.column_expression ");
+            sqlCmdBuilder.Append(" from all_indexes ind ");
+            sqlCmdBuilder.Append(" inner join all_ind_columns ind_col on ind.owner = ind_col.index_owner and ind.index_name = ind_col.index_name ");
+            sqlCmdBuilder.Append(" left outer join sys.all_ind_expressions ind_exp on ind.owner = ind_exp.index_owner and ind.index_name = ind_exp.index_name and ind_col.column_position = ind_exp.column_position ");
+            sqlCmdBuilder.Append(" where ind.owner not in ('ANONYMOUS','CTXSYS','DBSNMP','EXFSYS', 'LBACSYS', 'MDSYS', 'MGMT_VIEW', 'OLAPSYS', 'OWBSYS', 'ORDPLUGINS', 'ORDSYS', 'OUTLN','SI_INFORMTN_SCHEMA', 'SYS', 'SYSMAN', 'SYSTEM', 'TSMSYS', 'WK_TEST',");
+            sqlCmdBuilder.Append(" 'WKPROXY', 'WMSYS', 'XDB', 'APEX_040000', 'APEX_PUBLIC_USER', 'DIP', 'WKSYS',");
+            sqlCmdBuilder.Append(" 'FLOWS_30000', 'FLOWS_FILES', 'MDDATA', 'ORACLE_OCM', 'XS$NULL',");//-- excluding some Oracle maintained schemas
+            sqlCmdBuilder.Append(" 'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'PUBLIC') ");
+            sqlCmdBuilder.Append(" AND ind.table_name=:TABLE_NAME ");
+            sqlCmdBuilder.Append(" order by ind.table_owner,ind.table_name,ind.index_name,ind_col.column_position ");
+
+            DbCommand dbCmd = db.GetSqlStringCommand(sqlCmdBuilder.ToString());
+            dbCmd.CommandType = CommandType.Text;
+
+            db.AddInParameter(dbCmd, ":TABLE_NAME", DbType.AnsiString, tableName);
+
+            return db.ExecuteDataSet(dbCmd).Tables[0];
+        }
+    }
+}
---
+++ OracleToMySQLConvertor/Program.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleToMySQLConvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Database db = DatabaseFactory.CreateDatabase("ApplicationConnection");
+                DataTable tableList = DAL.GetTableList(db);
+
+                foreach(DataRow tableRow in tableList.Rows)
+                {
+                    string tableName = tableRow["TABLE_NAME"].ToString().ToUpper();
+                    Console.WriteLine("Converting Table " + tableName + "...");
+                    DataTable columnTable = DAL.GetTableColumns(db, tableName);
+                    DataTable primaryKeyTable = DAL.GetPrimaryKeyDetails(db, tableName);
+                    DataTable uniqueKeyTable = DAL.GetUniqueKeyDetails(db, tableName);
+                    DataTable indexTable = DAL.GetIndexDetails(db, tableName);
+
+                    StringBuilder schemaBuilder = new StringBuilder();
+                    schemaBuilder.AppendFormat("CREATE TABLE {0} \n(\n", tableName);
+                    int colIndex = 0;
+                    foreach(DataRow colRow in columnTable.Rows)
+                    {
+                        string columnName = colRow["COLUMN_NAME"].ToString().ToUpper();
+                        string dataType = colRow["DATA_TYPE"].ToString().ToUpper();
+                        int dataLength = 0;
+                        int.TryParse(colRow["DATA_LENGTH"].ToString(), out dataLength);
+                        int dataPrecision = 0;
+                        int.TryParse(colRow["DATA_PRECISION"].ToString(), out dataPrecision);
+                        int dataScale = 0;
+                        int.TryParse(colRow["DATA_SCALE"].ToString(), out dataScale);
+                        string nullable = colRow["NULLABLE"].ToString().ToUpper();
+                        string dataDefault = colRow["DATA_DEFAULT"].ToString().Length > 0 ? colRow["DATA_DEFAULT"].ToString() : "NULL";
+
+                        if(colIndex > 0)
+                            schemaBuilder.Append(",");
+
+                        if (dataType == "VARCHAR2")
+                            schemaBuilder.AppendFormat("{0}\tVARCHAR({1})", columnName, dataLength);
+                        else if(dataType == "NVARCHAR2")
+                            schemaBuilder.AppendFormat("{0}\tNVARCHAR({1})", columnName, dataLength);
+                        else if (dataType == "CHAR")
+                            schemaBuilder.AppendFormat("{0}\tCHAR({1})", columnName, dataLength);
+                        else if (dataType == "NUMBER" && dataScale == 0)
+                            schemaBuilder.AppendFormat("{0}\tINT", columnName);
+                        else if (dataType == "NUMBER" && dataScale > 0)
+                            schemaBuilder.AppendFormat("{0}\tDECIMAL({1},{2})", columnName, dataPrecision, dataScale);
+                        else if (dataType.Contains("TIMESTAMP"))
+                            schemaBuilder.AppendFormat("{0}\t{1}", columnName, dataType);
+
+                        if (nullable == "N")
+                            schemaBuilder.Append("\tNOT NULL");
+                        else
+                            schemaBuilder.AppendFormat("\tDEFAULT {0}", dataDefault);                            
+
+                        //add new line for each column
+                        schemaBuilder.Append("\n");
+                        colIndex++;
+                    }
+
+                    List<string> constraintList = new List<string>();
+
+                    if (primaryKeyTable.Rows.Count > 0)
+                    {
+                        string constraintName = primaryKeyTable.Rows[0]["CONSTRAINT_NAME"].ToString().ToUpper();
+                        List<string> columnList = new List<string>();
+                        foreach (DataRow primaryKeyRow in primaryKeyTable.Rows)
+                        {
+                            string columnName = primaryKeyRow["COLUMN_NAME"].ToString().ToUpper();
+                            columnList.Add(columnName);
+                        }
+                        schemaBuilder.AppendFormat(",CONSTRAINT {0} PRIMARY KEY ({1})\n", constraintName, string.Join(",", columnList));
+                        constraintList.Add(constraintName);
+                    }
+
+                    if (uniqueKeyTable.Rows.Count > 0)
+                    {
+                        DataTable uniqueKeys = uniqueKeyTable.DefaultView.ToTable(true, "CONSTRAINT_NAME");
+                        foreach(DataRow uniqueKeyRow in uniqueKeys.Rows)
+                        {
+                            string constraintName = uniqueKeyRow["CONSTRAINT_NAME"].ToString();
+                            DataRow[] uniqueKeyColumns = uniqueKeyTable.Select("CONSTRAINT_NAME='" + constraintName + "'", "POSITION ASC");
+                            List<string> columnList = new List<string>();
+                            foreach (DataRow row in uniqueKeyColumns)
+                            {
+                                string columnName = row["COLUMN_NAME"].ToString().ToUpper();
+                                columnList.Add(columnName);
+                            }
+                            schemaBuilder.AppendFormat(",CONSTRAINT {0} UNIQUE KEY ({1})\n", constraintName, string.Join(",", columnList));
+                            constraintList.Add(constraintName);
+                        }
+                    }
+
+                    schemaBuilder.Append(");");
+
+                    if (indexTable.Rows.Count > 0)
+                    {
+                        DataTable uniqueIndexs = indexTable.DefaultView.ToTable(true, "INDEX_NAME");
+                        foreach (DataRow uniqueIndexRow in uniqueIndexs.Rows)
+                        {
+                            string indexName = uniqueIndexRow["INDEX_NAME"].ToString().ToUpper();
+                            if (!constraintList.Contains(indexName))
+                            {
+                                DataRow[] indexColumns = indexTable.Select("INDEX_NAME='" + indexName + "'", "COLUMN_POSITION ASC");
+                                string indexType = string.Empty;
+                                string columnExpression = string.Empty;
+                                List<string> columnList = new List<string>();
+                                foreach (DataRow row in indexColumns)
+                                {
+                                    indexType = row["INDEX_TYPE"].ToString().ToUpper();
+                                    columnExpression = row["COLUMN_EXPRESSION"].ToString().ToUpper();
+                                    
+                                    string columnName = row["COLUMN_NAME"].ToString().ToUpper();
+                                    columnList.Add(columnName);
+                                }
+
+                                if (indexType != "FUNCTION-BASED NORMAL")
+                                    schemaBuilder.AppendFormat("\nCREATE INDEX {0} ON {1}({2});", indexName, tableName, string.Join(",", columnList));
+                                else
+                                    schemaBuilder.AppendFormat("\nCREATE INDEX {0} ON {1}({2});", indexName, tableName, columnExpression);
+                            }
+                        }
+                    }
+
+                    WriteToFile(tableName, schemaBuilder.ToString());
+                }
+
+                Console.WriteLine("Completed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void WriteToFile(string tableName, string content)
+        {
+            string fileName = "D:\\MySQL_DDL\\" + tableName + ".txt";
+            System.IO.File.WriteAllText(fileName, content);
+        }
+    }
+}
